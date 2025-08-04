e4fc1dcdd9f6aec12a968e71e5a10bf161457306
github-action-benchmark <github@users.noreply.github.com>
2025-08-04 06:16:31 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for e0ccc5e6972a55ae586d3bbd9adbbd73de47d943

--- benchmarks/network/data.js
+++ benchmarks/network/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1754288169005,
+  "lastUpdate": 1754288191026,
   "repoUrl": "https://github.com/bringhurst/snnd",
   "entries": {
     "Benchmark": [
@@ -73,6 +73,80 @@ window.BENCHMARK_DATA = {
           {
             "name": "simulation-throughput-neurons-small",
             "value": 0.718958,
+            "unit": "neurons/ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jon@bringhurst.org",
+            "name": "Jon Bringhurst",
+            "username": "bringhurst"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e0ccc5e6972a55ae586d3bbd9adbbd73de47d943",
+          "message": "Merge pull request #6 from bringhurst/redundant_benchmarks\n\nRemove redundant benchmarks",
+          "timestamp": "2025-08-03T23:15:36-07:00",
+          "tree_id": "0f83953b3c890045c5fd94aa19510b4e53669ca2",
+          "url": "https://github.com/bringhurst/snnd/commit/e0ccc5e6972a55ae586d3bbd9adbbd73de47d943"
+        },
+        "date": 1754288190148,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "total-simulation-time-medium",
+            "value": 13825.142918,
+            "unit": "ms"
+          },
+          {
+            "name": "initialization-time-medium",
+            "value": 976.182223,
+            "unit": "ms"
+          },
+          {
+            "name": "simulation-time-medium",
+            "value": 12848.960695,
+            "unit": "ms"
+          },
+          {
+            "name": "mean-firing-rate-medium",
+            "value": 54.355751,
+            "unit": "Hz"
+          },
+          {
+            "name": "active-neuron-ratio-medium",
+            "value": 1,
+            "unit": "ratio"
+          },
+          {
+            "name": "total-spikes-medium",
+            "value": 217423,
+            "unit": "count"
+          },
+          {
+            "name": "active-neurons-medium",
+            "value": 2000,
+            "unit": "count"
+          },
+          {
+            "name": "total-synapses-medium",
+            "value": 479805,
+            "unit": "count"
+          },
+          {
+            "name": "simulation-throughput-spikes-medium",
+            "value": 16.921446,
+            "unit": "spikes/ms"
+          },
+          {
+            "name": "simulation-throughput-neurons-medium",
+            "value": 0.155655,
             "unit": "neurons/ms"
           }
         ]
